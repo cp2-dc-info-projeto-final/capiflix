6dd98634110e0af32323265127aac672ed4c9a03
GabrielAngello <gabrielangelodlm@gmail.com>
2024-12-19 12:02:25 -0300

    controle de acesso

--- src/api/server.js
+++ src/api/server.js
@@ -19,6 +19,7 @@ const app = express();
 const port = 3000;
 
 app.use('/capas', express.static(path.join(__dirname, 'capas')));
+app.use(express.static(path.join(__dirname, 'frontend-app')));
 
 app.use(cors({
   origin: 'http://localhost:5173', // Habilita apenas URL do frontend svelte
@@ -142,47 +143,62 @@ async function verificaTokenAdmin(req, res, next) {
     });
   }
 
-  console.log(`token: ${token}`);
-  console.log(`SECRET_ACCESS_TOKEN: ${SECRET_ACCESS_TOKEN}`);
-  jwt.verify(token, SECRET_ACCESS_TOKEN, (err, decoded) => {
-    if (err) {
-      return res.status(401).json({
-        status: 'failed',
-        message: 'Sessão expirada!',
-      });
-    } else {
-      // o conteúdo decodificado do token é o id do usuário
-      let { idUsuario} = decoded;
-      console.log(`decoded: ${decoded}`);
-      console.log(`idUsuario decoded: ${decoded.idUsuario}`);
+app.get('/perfil', verificaToken, (req, res) => {
+  res.sendFile(path.join(__dirname, 'frontend-app/perfil.html'));
+});
 
-      db = geraConexaoDeBancoDeDados();
+app.get('/cadastro-filmes-admin', verificaTokenAdmin, (req, res) => {
+  res.sendFile(path.join(__dirname, 'frontend-app/cadastroFilmes.html'));
+});
 
-      // recupera dados do usuário que está tentando fazer login
-      db.get('SELECT id_usuario, nome, email FROM usuario WHERE id_usuario = ? AND is_admin = true', [idUsuario], async (error, result) => {
-        if (error) {
-          console.log(error)
-        }
-        else if (result) {
-          const { id_usuario, nome, email } = result
-          req.idUsuario = id_usuario
-          req.email = email
-          req.nome = nome
+app.get('/cadastro-generos', verificaTokenAdmin, (req, res) => {
+  res.sendFile(path.join(__dirname, 'frontend-app/cadastroGeneros.html'));
+});
 
-          db.close((err) => {
-            if (err) {
-              return console.error(err.message)
-            }
-            console.log('Fechou a conexão com o banco de dados.')
-          });
+app.get('/admin', verificaTokenAdmin, (req, res) => {
+  res.sendFile(path.join(__dirname, 'frontend-app/administrador.html'));
+});
 
-          next();
-        }
-      });
-    }   
-  });
+console.log(`token: ${token}`);
+console.log(`SECRET_ACCESS_TOKEN: ${SECRET_ACCESS_TOKEN}`);
+jwt.verify(token, SECRET_ACCESS_TOKEN, (err, decoded) => {
+  if (err) {
+    return res.status(401).json({
+      status: 'failed',
+      message: 'Sessão expirada!',
+    });
+  } else {
+    // o conteúdo decodificado do token é o id do usuário
+    let { idUsuario} = decoded;
+    console.log(`decoded: ${decoded}`);
+    console.log(`idUsuario decoded: ${decoded.idUsuario}`);
+
+    db = geraConexaoDeBancoDeDados();
+
+    // recupera dados do usuário que está tentando fazer login
+    db.get('SELECT id_usuario, nome, email FROM usuario WHERE id_usuario = ? AND is_admin = true', [idUsuario], async (error, result) => {
+      if (error) {
+        console.log(error)
+      }
+      else if (result) {
+        const { id_usuario, nome, email } = result
+        req.idUsuario = id_usuario
+        req.email = email
+        req.nome = nome
+
+        db.close((err) => {
+          if (err) {
+            return console.error(err.message)
+          }
+          console.log('Fechou a conexão com o banco de dados.')
+        });
+
+        next();
+      }
+    });
+  }   
+ });
 }
-
 
 // esta função é um middleware, uma chamada que vai entre duas chamadas para verificar se o usuário está logado
 async function verificaToken(req, res, next) {  
@@ -672,7 +688,7 @@ app.post('/filmes/novo', capa.single('imagem'), (req, res) => {
   const { titulo, descricao, ano, classificacao, id_genero } = req.body;
   let erro = "";
 
-  // Validação dos campos do formulário
+  // Validação dos campos verdo formulário
   if (!titulo || !descricao || !ano || !classificacao || !id_genero) {
     erro += 'Por favor, preencha todos os campos corretamente!';
   }
